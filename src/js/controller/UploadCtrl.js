21c248ef1301a74dff1ce1497987670629b9b2c0
YukiRai <kevin.santamaria@viacesi.fr>
2016-10-03 10:49:20 +0200

    TODO 3 : Ajout bouton Home + réorganisation des boutons

--- src/js/controller/UploadCtrl.js
+++ src/js/controller/UploadCtrl.js
@@ -2,25 +2,26 @@ var app = require('../app.js');
 app
 .controller('UploadCtrl', ['$scope', '$http', function ($scope, $http) {
   console.log("controller upload");
-  
+
   // TODO : coder le local parse
   $scope.form = {};
-  
+
   $scope.parseAndPost = function(file){
     var fileContent = file.split('\n');
     fileContent = _.map(fileContent, function(l){
       var data = l.split(',');
       data = _.map(data, function(s){return s.trim()});
+      console.log(data);
       return data;
     })
-    
+
     _.each(fileContent, function(data){//<==> .on("data", function(data){
-      
+
     });
-    
-    
+
+
     console.log(fileContent);
   }
-  
-  
+
+
 }]);
