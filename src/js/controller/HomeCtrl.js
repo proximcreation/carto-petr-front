bb065d9a0669338aeca0a0ce54182096904d31de
YukiRai <kevin.santamaria@viacesi.fr>
2016-10-17 16:48:38 +0200

    affichage des items avec couleur dépendant de leur type (trois couleurs), mise en place checkbox par type, et début d'implémentation de l'affichage des items en fonction de la checkbox coché/pas coché

--- src/js/controller/HomeCtrl.js
+++ src/js/controller/HomeCtrl.js
@@ -4,16 +4,18 @@ app
 .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
   var mymap = L.map('petr').setView([43.776600, 1.435513], 11);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJveGltY3JlYXRpb24iLCJhIjoiY2lzZXVjaXd5MDAydDJ1bnF6N2U0NHp1cSJ9.7w32xitQZ8iFA28qdQIvvw', {
+  var map = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJveGltY3JlYXRpb24iLCJhIjoiY2lzZXVjaXd5MDAydDJ1bnF6N2U0NHp1cSJ9.7w32xitQZ8iFA28qdQIvvw', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'your.mapbox.project.id',
     accessToken: 'your.mapbox.public.access.token'
   }).addTo(mymap);
+  map.bringToBack();
   $scope.life = {};
   $scope.new = {};
+
   function onEachFeature(feature, layer) {
-    var popupContent = "<p>INFOS :</p>";
+    var popupContent = "<center> informations :</center>";
 
     if (feature.properties && feature.properties.popupContent) {
       popupContent += feature.properties.popupContent;
@@ -21,12 +23,8 @@ app
 
     layer.bindPopup(popupContent);
 
-    layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: zoomToFeature
-    });
   }
+
   $scope.petrarea = [];
   $http.get("http://localhost:1337/city/?limit=80")
   .then(function (response) {
@@ -55,6 +53,7 @@ app
       }
     );
     geojson.addTo(mymap);
+    geojson.bringToBack();
   });
 
   $scope.hgarea = [];
@@ -82,58 +81,69 @@ app
     geojson.addTo(mymap);
   });
 
-  var greenIcon = L.icon({
-    iconUrl: 'greenmark.png',
-    shadowUrl: 'marker-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-  });
-
+  $scope.itempoints = [];
   $http.get("http://localhost:1337/item/?limit=80")
   .then(function (response) {
     for (i=0; i<response.data.length; i++){
       var Coord = response.data[i].coordinates[0].split(",");
-      var geojsonitem =  {
+      $scope.itempoints.push ({
         "type": "Feature",
         "properties": {
           "type": response.data[i].type,
           "name": response.data[i].name,
-          "popupContent": response.data[i].description + "\n" + response.data[i].address + "\n" + response.data[i].horaires
+          "popupContent":
+                "<center><h2>" + response.data[i].name + "</h2></center>"
+              + "<center><strong>" + response.data[i].description + "<br><br></strong></center>"
+              + "adresse : <i>" + response.data[i].address + "</i><br><br>"
+              + response.data[i].horaires
         },
         "geometry": {
           "type": "Point",
-          "coordinates": [parseFloat(Coord[0]),parseFloat(Coord[1])]  // Probleme des coordinates, elles ont des guillemets qu'il faut absolument supprimer, sinon le Geojson peut pas etre consruit.
+          "coordinates": [parseFloat(Coord[1]),parseFloat(Coord[0])]
         }
-      };
-      // L.geoJson(geojsonitem, {
-      //   onEachFeature: onEachFeature
-      // }).addTo(mymap);
+      });
     }
+
+    // $scope.$watch(
+    //   // This function returns the value being watched. It is called for each turn of the $digest loop
+    //   function() { return checki; },
+    //   // This is the change listener, called when the value returned from the above function changes
+    //   function(newValue, oldValue) {
+    //     if ( newValue !== oldValue ) {
+    //       // Only increment the counter if the value changed
+    //       console.log("afficher/desafficher")
+    //     }
+    //   }
+    // );
+
+    var gitem = L.geoJson($scope.itempoints, {
+      pointToLayer: function (feature, latlng) {
+        var typecolor;
+        switch (feature.properties.type) {
+          case 'touristic':
+          typecolor = "#e22121"
+          break;
+          case 'institution':
+          typecolor = "#1e7ad0"
+          break;
+          case 'entreprise':
+          typecolor = "#dbee0c"
+          break;
+          default:
+          typecolor = "#4aa989"
+        }
+        return L.circleMarker(latlng, {
+          radius: 5,
+          fillColor: typecolor,
+          color: "#000",
+          weight: 1,
+          opacity: 0.9,
+          fillOpacity: 0.8
+        });
+      },
+      onEachFeature: onEachFeature
+    }).addTo(mymap);
+    gitem.bringToFront();
   });
-
-          // switch ($scope.itempoints.properties.type) {
-          //   case 'touristic': return {iconUrl: 'greenmark.png'};
-          //   case 'institution':   return {iconUrl: 'yellowmark.png'};
-          //   case 'entreprise':   return {iconUrl: 'bluemark.png'};
-          // }
-          // radius: 8,
-          // fillColor: "#ff7800",
-          // weight: 1,
-          // opacity: 1,
-          // fillOpacity: 0.8
-
-//     var greenIcon = L.icon({
-//     iconUrl: 'greenmark.png',
-//     shadowUrl: 'marker-shadow.png',
-//
-//     iconSize:     [38, 95], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
+  console.log($scope.itempoints);
 }]);
